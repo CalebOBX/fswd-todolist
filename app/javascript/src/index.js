99c1c2fe7e2942d9bcb5e0bc98863cbfaa2eb88a
Caleb Burrus <calebburrus@gmail.com>
2024-02-23 21:57:26 +0000

    mark complete/active working

--- app/javascript/src/index.js
+++ app/javascript/src/index.js
@@ -9,9 +9,34 @@ import {
 } from "./requests.js";
 
 indexTasks(function (response) {
+
   var htmlString = response.tasks.map(function (task) {
-    return "<div class='col-12 mb-3 p-2 border rounded task' data-id='" + task.id + "'>" + task.content + "</div>";
+    if (!task.completed) {
+      return "<div class='col-12 mb-3 p-2 border rounded task'>" + 
+              "<input type='checkbox' data-id='" + task.id + "' />" +
+              "<span>" + task.content + "</span>" +
+              "<button id='deleteTask' data-id='" + task.id + "' class='btn btn-danger float-right delete'>Delete</button>" +
+              "</div>";
+    }
+    else if (task.completed) {
+      return "<div class='col-12 mb-3 p-2 border rounded task'>" + 
+              "<input type='checkbox' data-id='" + task.id + "' checked />" +
+              "<span>" + task.content + "</span>" +
+              "<button id='deleteTask' data-id='" + task.id + "' class='btn btn-danger float-right delete'>Delete</button>" +
+              "</div>";
+    };
   });
 
   $("#tasks").html(htmlString);
+});
+
+// update task for completed or active by checkbox status
+$(document).on('click', 'input[type=checkbox]', function (event) {
+  let checkbox = $(event.target);
+  if (checkbox.is(':checked')) {
+    markTaskComplete(checkbox.attr('data-id'));
+  }
+  else {
+    markTaskActive(checkbox.attr('data-id'));
+  }
 });
